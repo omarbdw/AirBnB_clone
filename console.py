c69532c38ce79b6ef4ecedb1eb58ad0afdb3e7da
omarbdw <omarbdw@yahoo.com>
2024-01-09 11:27:31 +0200

    test

--- console.py
+++ console.py
@@ -1,34 +1,20 @@
-""" contains the entry point of the command interpreter """
+#!/usr/bin/python3
 import cmd
-import re
-from shlex import split
-from models import storage
-from models.base_model import BaseModel
-
 
 class HBNBCommand(cmd.Cmd):
-    """ class HBNBCommand """
     prompt = '(hbnb) '
-    classes = ["BaseModel"]
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """Exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
+        """Exit the program"""
         return True
 
     def emptyline(self):
-        """empty line + ENTER shouldn’t execute anything"""
+        """Do nothing when an empty line is entered"""
         pass
 
-    def do_create(self, arg):
-        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
-        if not arg:
-            print("** class name missing **")
-        elif arg not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-        else:
-            new = eval(arg)
-    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
